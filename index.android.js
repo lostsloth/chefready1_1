b6e3cc103e9c76efd53e06c1b8f4e4c69f6cc071
lostsloth <greggrossi@Gregs-MacBook-Pro.local>
2017-05-15 18:08:35 -0600

    update android file
    android file is identical to iOS

--- index.android.js
+++ index.android.js
@@ -9,23 +9,33 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableOpacity
 } from 'react-native';
+import fbsdk, { LoginManager }  from 'react-native-fbsdk'
 
 export default class chefready1_1 extends Component {
+  
+  _fbAuth (){
+    LoginManager.logInWithReadPermissions(['public_profile']).then(function(result){
+        if(result.isCancelled){
+          console.log('Login Cancelled');
+        }else {
+          console.log('Login success: ' + result.grantedPermissions);
+        }
+    }, function(error) {
+          console.log('An error occurred: ' + error);
+        })
+  }
+  
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+        <TouchableOpacity onPress={this._fbAuth}>
+          <Text>
+            Login with fakebook
+          </Text>
+          </TouchableOpacity>
       </View>
     );
   }
